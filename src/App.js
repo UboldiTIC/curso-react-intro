53ecf030d15403163059c8a756557a62e31cbff0
Solmar <solmaruboldi@gmail.com>
2024-12-26 18:35:08 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter /> {/* Importamos un componente dentro de otro componente. */}
+      <TodoSearch />
+      <TodoList>
+        <TodoItem /> 
+        <TodoItem />
+        <TodoItem />
+      </ TodoList>  
+
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
   );
 }
 
