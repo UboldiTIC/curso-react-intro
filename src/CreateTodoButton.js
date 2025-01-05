c4122a3b940646f934b39447b80d0b979fcd0c20
Tuta <solmaruboldi@gmail.com>
2025-01-05 14:00:37 -0300

    Update CreateTodoButton and TodoSearch with events

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,16 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return (
-      <button className='create-todo-button'  >+</button>
+      <button 
+        className='create-todo-button'  
+        onClick={                        // Agregamos un evento onClick al botón - Interacción siempre con on.
+          (event) => {
+            console.log('le diste click')
+            console.log(event)
+            console.log(event.target)
+          }
+        }
+      >+</button>
     );
   }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    // Creamos un estado, un arrglo que no sólo se consume, sino que también se actualiza con una funcion acutalizadora.
+    // Es decir, para un state llamado state, se crea una función llamada setState.
+    
+    // A continuación, agregamos un estado para guardar el valor del input.
+    const [searchValue, setSearchValue] = React.useState(''); 
+
+    console.log("Los usuarios buscan todos de: " + searchValue);
+
     return (
-        
         <input 
             placeholder="Cortar cebolla" 
-            className="todo-search" 
+            className="todo-search"
+            value={searchValue}
+            onChange={(event) => {
+                //console.log('Cambio en el input search');
+                //console.log(event.target.value);
+                setSearchValue(event.target.value);
+            }}
         />
     );  
 }
