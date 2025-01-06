a0a04230bccd2ab50ab3d8c9d0b98c7323b7a947
Tuta <solmaruboldi@gmail.com>
2025-01-06 12:26:23 -0300

    Update todoItems witch interactive icons

--- src/App.js
+++ src/App.js
@@ -10,21 +10,60 @@ const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
-  
+  { text: 'Abrir el grifo', completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  // Agregamos el estado
+  const [searchValue, setSearchValue] = React.useState(''); 
+  
+  const completedTodos = todos.filter(todo => !!todo.completed).length; // !! la doble negación convierte a booleano.
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => { 
+    const textTodo = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return textTodo.includes(searchText);
+    }
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
+  console.log("Los usuarios buscan todos de: " + searchValue);
+
   return (
     <React.Fragment>
-      <TodoCounter completed={16} total={25} /> {/* Importamos un componente dentro de otro componente. */}
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} /> {/* Importamos un componente dentro de otro componente. */}
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {defaultTodos.map(todo => ( 
+        {searchedTodos.map(todo => ( 
           < TodoItem 
             key={todo.text} 
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>  
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({
     // Creamos un estado, un arrglo que no sólo se consume, sino que también se actualiza con una funcion acutalizadora.
     // Es decir, para un state llamado state, se crea una función llamada setState.
     
     // A continuación, agregamos un estado para guardar el valor del input.
-    const [searchValue, setSearchValue] = React.useState(''); 
 
-    console.log("Los usuarios buscan todos de: " + searchValue);
+    /* const [searchValue, setSearchValue] = React.useState(''); 
 
+    console.log("Los usuarios buscan todos de: " + searchValue); */
+
+    // El estado se debe comunicar con su componente padre, en este caso App.js. Pero no se puede hacer ya que sólo 
+    // se puede comunicar con sus hijos. Por ello, lo cortamos y lo llevamos a App.js.
+
+    // Ahora, vamos a recibir el estado y la función actualizadora como props.
+
+    searchValue,
+    setSearchValue,
+}) {
     return (
         <input 
             placeholder="Cortar cebolla" 
